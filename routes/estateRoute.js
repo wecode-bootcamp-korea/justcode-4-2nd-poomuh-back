19116a12b001ccc3c65e14e8bfdd8d3e4f9736a9
shorry <ksb334ksb@gmail.com>
2022-04-25 19:56:06 +0900

    ADD : map filtering like 값 보여주기 추가2

--- routes/estateRoute.js
+++ routes/estateRoute.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const estateController = require('../controllers/estateController');
+const { usersValidateToken } = require('../middlewares/usersValidateToken');
 
 router.get('/', estateController.filteredMaps);
+router.get('/users', usersValidateToken, estateController.filteredMaps);
 
 module.exports = router;
